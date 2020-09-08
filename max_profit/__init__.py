6e177e04e5044bce76bc48f23c40b0be63e0297e
andy shaw <shawry@shawry.com>
2020-09-08 14:45:52 +1000

    Update test cases

--- max_profit/__init__.py
+++ max_profit/__init__.py
@@ -4,9 +4,10 @@ def max_profit(prices):
     min_price = prices[0]
 
     for i in range(1, len(prices)):
-        if (prices[i] - min_price > max_profit): 
-            max_profit = prices[i] - min_price 
+        if (prices[i] - min_price > max_profit):
+            max_profit = prices[i] - min_price
 
-        if (prices[i] < min_price): 
-            min_price = prices[i] 
-    return max_profit 
+        if (prices[i] < min_price):
+            min_price = prices[i]
+
+    return max(max_profit, 0)
--- tests/test_max_profit.py
+++ tests/test_max_profit.py
@@ -3,24 +3,33 @@ from max_profit import max_profit
 
 class TestMaxProfit(unittest.TestCase):
     def test_exceptions(self):
+        """
+        Test that raises an exception with insufficient data
+        """
         data = [1]
         self.assertRaises(Exception, max_profit, data)
 
     def test_max_profit_simple(self):
+        """
+        Test that it works with the bare minimum data
+        """
         data = [1, 100]
         result = max_profit(data)
         self.assertEqual(result, 99)
 
     def test_max_profit_complex(self):
         """
-        Test that it can sum a list of integers
+        Test that it works on the sample data
         """
         data = [10, 7, 5, 8, 11, 9]
         result = max_profit(data)
         self.assertEqual(result, 6)
 
-    def test_max_profit_break_even(self):
-        data = [10, 10]
+    def test_max_profit_none(self):
+        """
+        Test that it works when no profit opportunity exists
+        """
+        data = [10, 5]
         result = max_profit(data)
         self.assertEqual(result, 0)
 
